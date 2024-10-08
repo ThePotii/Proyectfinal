b5fd7ba6ec4754702fcb4864d0aa2e3f132be92a
yosoybart <yosoybart03@gmail.com>
2024-10-08 17:26:15 -0400

    mejorado, alinear detalles.

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 let contenedor = document.getElementById('listamusical');
-
-
-// Tienes el código repetido y se estaba ejecutando dos veces
-// axios.get('https://api.institutoalfa.org/api/songs').then((res) => {
-    //     res.data.songs.map((song) => {
-        //         let div = document.createElement('div')
-        //         div.setAttribute('class', 'flex p-4 gap-2')
-        
-        //         div.innerHTML = `
-        //            <img src="https://api.institutoalfa.org/api/songs/image/${song.image.filename}" class="rounded-full h-16" alt="">
-        //                 <div>
-        //                     <h3 class= "font-bold">${song.title}</h3>
-        //                     <p>${song.author}</p>
-        //                 </div>
-        
-        //         `
-        //         contenedor.appendChild(div)
-//     })
-// })
-
-
 axios.get('https://api.institutoalfa.org/api/songs').then((res) => {
     res.data.songs.map((albumpopular) => {
         let div = document.createElement('div')
